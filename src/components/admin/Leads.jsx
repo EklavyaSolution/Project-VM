d98fd3a25a41fea1728d9ca881c6070f1ea2c0c5
Anil <Anilkrsingh1561@gmail.com>
2024-07-03 13:08:43 +0530

    configured leads page with api

--- src/components/admin/Leads.jsx
+++ src/components/admin/Leads.jsx
@@ -1,5 +1,6 @@
 "use client"
 
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
 const Leads = () => {
@@ -14,6 +15,15 @@ const Leads = () => {
         }
     };
 
+    const formatDate = (date) => {
+        const prevdate = new Date(date);
+        const newDate = prevdate.toLocaleDateString('en-US', {
+            year: "2-digit",
+            month: "short",
+            day: "numeric"
+        })
+        return newDate
+    }
     useEffect(() => {
         fetchLeads();
     }, [])
@@ -24,27 +34,32 @@ const Leads = () => {
             <div className=' overflow-y-auto overflow-x-hidden h-fit flex-wrap mb-10 pb-10 px-10'>
                 <table className='bg-[#efefef] w-full divide-y-2 divide-[#aaa] text-center font-nunito font-medium text-[17px]'>
                     <thead>
-                        <th className='pt-2 pb-1'>S. No.</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Mobile</th>
-                        <th>Birth Date</th>
-                        <th>Birth Time</th>
-                        <th>Birth Place</th>
-                        <th>Message</th>
+                        <tr className='pt-2 pb-1'>
+                            <th>S. No.</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Mobile</th>
+                            <th>Birth Date</th>
+                            <th>Birth Time</th>
+                            <th>Birth Place</th>
+                            <th>Message</th>
+                        </tr>
                     </thead>
-                    {leadsData?.map((item, index) => {
-                        return <tbody key={index}>
-                            <td className='py-2'>{index}</td>
-                            <td>{item.name}</td>
-                            <td>{item.email}</td>
-                            <td>{item.mobile}</td>
-                            <td>{item.birthDate}</td>
-                            <td>{item.birthTime}</td>
-                            <td>{item.birthPlace}</td>
-                            <td className='w-72'>{item.message}</td>
-                        </tbody>
-                    })}
+                    <tbody>
+                        {leadsData?.map((item, index) => {
+                            return <tr key={index}>
+                                <td className='py-2'>{index + 1}</td>
+                                <td>{item.name}</td>
+                                <td>{item.email}</td>
+                                <td>{item.mobile}</td>
+                                <td>{formatDate(item.birthDate)}</td>
+                                <td>{item.birthTime}</td>
+                                <td>{item.birthPlace}</td>
+                                <td className='w-64'>{item.message}</td>
+                            </tr>
+                        })}
+                    </tbody>
+
 
                 </table>
             </div>
